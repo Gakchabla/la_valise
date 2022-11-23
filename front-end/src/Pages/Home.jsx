c880840f2c300c0d03f9d781028f85180bb78388
Dreyrk <lucas.rondepierre123@gmail.com>
2022-11-23 19:11:50 +0100

    branche dev

--- front-end/src/Pages/Home.jsx
+++ front-end/src/Pages/Home.jsx
@@ -6,6 +6,7 @@ function Home() {
       <h1 className="flex justify-center text-2xl text-red-400">
         Welcome to the Wild Hackathon !
       </h1>
+      <h2>Branche dev</h2>
     </div>
   );
 }
